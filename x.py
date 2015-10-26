1be0711474c275bb0de6849ef2e9dddad6faa209
kparsa <kparsa@contextualgenomics.com>
2015-10-26 02:09:00 -0700

    cleanup

--- x.py
+++
@@ -1,5 +0,0 @@
-from parallel_sync import executor
-creds={'user': 'kparsa', 'key':'~/.ssh/id_rsa', 'host':'192.168.168.31'}
-#print executor.run('pwd', creds=creds, curr_dir='/tmp')
-executor.delete_dir('/tmp/qq', creds=creds)
-
